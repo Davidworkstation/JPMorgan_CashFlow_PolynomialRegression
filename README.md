47a3ec536ee047986bf701bbf5c133a4acacaef6
Risaiah <db2019@hawaii.edu>
2024-06-09 04:59:30 -0400

    update

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# JPMorgan_CashFlow_PolynomialRegression
- Currently Unfinished, but for the purpose of the model I uploaded it.,
+# JP Morgan Cash Flow Data Polynomial Fitting
+Polynomial fitting of time series data involves using polynomial functions to approximate and model the underlying trend within the data. This method aims to capture the overall pattern or shape of the time series by fitting a polynomial equation to the data points. The degree of the polynomial determines the complexity of the model, with higher degrees offering more flexibility but risking overfitting. Polynomial fitting can be useful for smoothing noisy data, identifying long-term trends, or making short-term predictions. However, it's important to be cautious of overfitting and to consider the appropriateness of the polynomial degree based on the characteristics of the data and the desired modeling goals.
+
+## Installation
+To install the project dependencies, run:
+pip install -r requirements.txt
+
+## Usage
+Basic instructions on how to use the project:
+
+On your computer, navigate to the repository's folder and type the following in the terminal:
+streamlit run polyregress.py
+
+note: depending on your python setup, you may have to include 'python' or 'python3' in front of the command.
+
+
+## Project Structure
+Overview of the project's structure:
+
+project-root/
+
+├── py/ - polyregress - 
+
+├── py/ - webscrape - 
+
+├── csv/ - JPcashflow - 
+
+├── requirements.txt - Project dependencies
+
+├── .gitatributes - Text handling
+
+└── README.md - This file
+
+## Contact Information
+For questions, please contact David Bannister at (https://www.linkedin.com/in/david-bannister-230a67191/).
--- polyregress.py
+++ polyregress.py
@@ -4,47 +4,88 @@ import matplotlib.dates as mdates
 import plotly.graph_objects as go
 import streamlit as st
 
+st.set_page_config(layout='wide')
+st.markdown(
+    f"""
+    <h1 style='text-align: center;'>JP Morgan Cash Flow Data Polynomial Analysis</h1>
+    """,
+    unsafe_allow_html=True
+)
+
 JPmorgan_Cashflow_data = pd.read_csv(r"/Users/risaiah/Desktop/GitHub Repositories/Data-Science-Econometrics-Personal/Polynomial/JPcashflow.csv")
 JPmorgan_Cashflow_data = JPmorgan_Cashflow_data.drop(1)
 
+with st.expander('Preview Data'):
+    st.dataframe(JPmorgan_Cashflow_data)
+    st.caption('Values of \'-\' are replaced with 0 for simplicity.')
+
+col1, colraw, col2, col3  =  st.columns([5,1,9,9])
+selecteddate=col1.selectbox('Variable Name',JPmorgan_Cashflow_data['Column1'][1:])
+
+x_int = list(JPmorgan_Cashflow_data['Column1']).index(selecteddate)
 JPtimedata = pd.to_datetime(JPmorgan_Cashflow_data.iloc[0][3:])
-JProwdata = JPmorgan_Cashflow_data.iloc[7][3:]
-JProwdata = JProwdata.apply(lambda x: x.replace("$","").replace(",",""))
+JProwdata = JPmorgan_Cashflow_data.iloc[x_int][3:]
+JProwdata = JProwdata.apply(lambda x: x.replace("$","").replace(",","").replace('-','0'))
 JProwdata = pd.to_numeric(JProwdata)
 length_ofdata = np.arange(len(JProwdata))
-data_variable_name = JPmorgan_Cashflow_data.iloc[7][0]
+data_variable_name = JPmorgan_Cashflow_data.iloc[x_int][0]
 
 JPtimedata_reference = mdates.date2num(JPtimedata)
-P = np.polyfit(JPtimedata_reference, JProwdata, 9)
-polyfit_values = np.polyval(P, JPtimedata_reference)
+
+def polyfitplot(polyfiti, titl=True):
+    P = np.polyfit(JPtimedata_reference, JProwdata, polyfiti)
+    polyfit_values = np.polyval(P, JPtimedata_reference)
+
+    fig = go.Figure()
+
+    actual_data = go.Scatter(
+        x=JPtimedata,
+        y=JProwdata,
+        mode='lines',
+        name='Actual Data'
+    )
+
+    polyfit_data = go.Scatter(
+        x=JPtimedata,
+        y=polyfit_values,
+        mode='lines',
+        name='Polynomial Fit'
+    )
+    
+    if titl == False:
+        fig.update_layout(
+            title=f"Fitted at Polynomial = {polyfiti}",
+            title_x=0.3,
+            title_y=0.95
+        )
+        
+    else:
+        fig.update_layout(
+            title=f"{selecteddate} Fitted at Polynomial = {polyfiti}",
+            title_x=0.3,
+            title_y=0.95
+        ) 
+
+    fig.update_xaxes(title_text = "Dates")
+    fig.update_yaxes(title_text = "Dollars (Trillions)")
+
+    fig.add_trace(actual_data)
+    fig.add_trace(polyfit_data)
+    
+    fig.update_layout(width=450, height=250)
+
+    return fig
+
+a=col1.number_input('Graph 1 Polynomial', step=1, min_value=0)
+b=col1.number_input('Graph 2 Polynomial', step=1, min_value=0)
+c=col1.number_input('Graph 3 Polynomial', step=1, min_value=0)
+d=col1.number_input('Graph 4 Polynomial', step=1, min_value=0)
+yep = col1.checkbox('Title Display?')
+
+col2.plotly_chart(polyfitplot(a, titl=yep))
+col3.plotly_chart(polyfitplot(b, titl=yep))
+col2.plotly_chart(polyfitplot(c, titl=yep))
+col3.plotly_chart(polyfitplot(d, titl=yep))
 
 
-fig = go.Figure()
-
-actual_data = go.Scatter(
-    x=JPtimedata,
-    y=JProwdata,
-    mode='lines',
-    name='Actual Data'
-)
-
-polyfit_data = go.Scatter(
-    x=JPtimedata,
-    y=polyfit_values,
-    mode='lines',
-    name='Polynomial Fit'
-)
-
-fig.update_layout(
-    title=f"{data_variable_name} Time Series Data",
-    title_x=0.3,
-    title_y=0.95
-)
-
-fig.update_xaxes(title_text = "Dates")
-fig.update_yaxes(title_text = "Dollars (Trillions)")
-
-fig.add_trace(actual_data)
-fig.add_trace(polyfit_data)
-
-fig.show()
+st.caption('Thank you for viewing. Please contact db2019@hawaii.edu if you have any questions. Copyrights reserved.')
